73d1f48c539c4bc27f41e902a2923124d4db932e
jeongjoonsup <penny4860@gmail.com>
2017-12-09 18:51:07 +0900

    yolo.predict module working

--- predict.py
+++ predict.py
@@ -4,12 +4,8 @@ import argparse
 import os
 import cv2
 import numpy as np
-from tqdm import tqdm
-import json
-from yolo.preprocessing import parse_annotation
-from yolo.utils import draw_boxes
-from yolo.frontend import YOLO
 
+from yolo.predict import predict
 
 DEFAULT_CONFIG_FILE = "config.json"
 DEFAULT_PRETRAINED_WEIGHTS = "mobilenet_raccoon.h5"
@@ -40,69 +36,6 @@ argparser.add_argument(
     default=DEFAULT_INPUT_IMAGE,
     help='path to an image or an video (mp4 format)')
 
-def _main_(args):
- 
-    config_path  = args.conf
-    weights_path = args.weights
-    image_path   = args.input
-
-    with open(config_path) as config_buffer:    
-        config = json.load(config_buffer)
-
-    ###############################
-    #   Make the model 
-    ###############################
-
-    yolo = YOLO(architecture        = config['model']['architecture'],
-                input_size          = config['model']['input_size'], 
-                labels              = config['model']['labels'], 
-                max_box_per_image   = config['model']['max_box_per_image'],
-                anchors             = config['model']['anchors'])
-
-    ###############################
-    #   Load trained weights
-    ###############################    
-
-    print(weights_path)
-    yolo.load_weights(weights_path)
-
-    ###############################
-    #   Predict bounding boxes 
-    ###############################
-
-    if image_path[-4:] == '.mp4':
-        video_out = image_path[:-4] + '_detected' + image_path[-4:]
-
-        video_reader = cv2.VideoCapture(image_path)
-
-        nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
-
-        video_writer = cv2.VideoWriter(video_out,
-                               cv2.VideoWriter_fourcc(*'MPEG'), 
-                               50.0, 
-                               (frame_w, frame_h))
-
-        for i in tqdm(list(range(nb_frames))):
-            _, image = video_reader.read()
-            
-            boxes = yolo.predict(image)
-            image = draw_boxes(image, boxes, config['model']['labels'])
-
-            video_writer.write(np.uint8(image))
-
-        video_reader.release()
-        video_writer.release()  
-    else:
-        image = cv2.imread(image_path)
-        boxes = yolo.predict(image)
-        image = draw_boxes(image, boxes, config['model']['labels'])
-
-        print(len(boxes), 'boxes are found')
-
-        cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], image)
-
 if __name__ == '__main__':
     def test_predict():
         def is_equal_image_file(output_file, desired_file):
@@ -121,7 +54,7 @@ if __name__ == '__main__':
         args.weights = "tests//mobilenet_raccoon.h5"
         args.input = "tests//raccoon.jpg"
 
-        _main_(args)
+        predict(args.input, args.weights, args.input)
 
         # 3. Should 
         return is_equal_image_file(output_file, desired_file)
