9f209096d4100a4aedd9fd0b19cbbe756126a306
jeongjoonsup <penny4860@gmail.com>
2017-12-09 19:21:24 +0900

    test code clening

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -8,30 +8,47 @@ import os
 
 SAMPLE_DIRECTORY = "..//sample"
 
+@pytest.fixture(scope='function')
+def setup_inputs(request):
+    input_file = os.path.join(SAMPLE_DIRECTORY, "raccoon.jpg")
+    conf = os.path.join(SAMPLE_DIRECTORY, "config.json")
+    weights = os.path.join(SAMPLE_DIRECTORY, "mobilenet_raccoon.h5")
+    return input_file, conf, weights
 
-def test_predict():
+
+@pytest.fixture(scope='function')
+def setup_outputs(request):
+    def _delete_file(fname):
+        if os.path.exists(fname):
+            os.remove(fname)
+    output_file = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected.jpg")
+    desired_file = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected_gt.jpg")
+    _delete_file(output_file)
+    def teardown():
+        _delete_file(output_file)
+    request.addfinalizer(teardown)
+    return output_file, desired_file
+
+
+def test_predict(setup_inputs, setup_outputs):
     def is_equal_image_file(output_file, desired_file):
         output_img = cv2.imread(output_file)
         desired_img = cv2.imread(desired_file)
         return np.allclose(output_img, desired_img)
 
     # 1. Given 
-    output_file = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected.jpg")
-    desired_file = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected_gt.jpg")
-    if os.path.exists(output_file):
-        os.remove(output_file)
+    input_file, conf, weights = setup_inputs
+    output_file, desired_file = setup_outputs
+    
     # 2. When run main()
-    conf = os.path.join(SAMPLE_DIRECTORY, "config.json")
-    weights = os.path.join(SAMPLE_DIRECTORY, "mobilenet_raccoon.h5")
-    predict(os.path.join(SAMPLE_DIRECTORY, "raccoon.jpg"),
-            weights,
-            conf)
+    predict(input_file, weights, conf)
 
     # 3. Should 
-    return is_equal_image_file(output_file, desired_file)
+    assert(is_equal_image_file(output_file, desired_file) == True)
+    
     
 if __name__ == '__main__':
-    pytest.main([__file__])
+    pytest.main([__file__, "-v", "-s"])
 
 
     
