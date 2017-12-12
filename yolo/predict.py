3dfa8ff23a7d379a1c49f4dc15477589df06305c
jeongjoonsup <penny4860@gmail.com>
2017-12-12 19:56:59 +0900

    predict driver mp4 format deleted

--- yolo/predict.py
+++ yolo/predict.py
@@ -32,36 +32,10 @@ def predict(image_path, weights_path, config_path="config.json"):
     ###############################
     #   Predict bounding boxes 
     ###############################
+    image = cv2.imread(image_path)
+    boxes = yolo.predict(image)
+    image = draw_boxes(image, boxes, config['model']['labels'])
 
-    if image_path[-4:] == '.mp4':
-        video_out = image_path[:-4] + '_detected' + image_path[-4:]
+    print(len(boxes), 'boxes are found')
 
-        video_reader = cv2.VideoCapture(image_path)
-
-        nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
-
-        video_writer = cv2.VideoWriter(video_out,
-                               cv2.VideoWriter_fourcc(*'MPEG'), 
-                               50.0, 
-                               (frame_w, frame_h))
-
-        for i in tqdm(list(range(nb_frames))):
-            _, image = video_reader.read()
-            
-            boxes = yolo.predict(image)
-            image = draw_boxes(image, boxes, config['model']['labels'])
-
-            video_writer.write(np.uint8(image))
-
-        video_reader.release()
-        video_writer.release()  
-    else:
-        image = cv2.imread(image_path)
-        boxes = yolo.predict(image)
-        image = draw_boxes(image, boxes, config['model']['labels'])
-
-        print(len(boxes), 'boxes are found')
-
-        cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], image)
+    cv2.imwrite(image_path[:-4] + '_detected' + image_path[-4:], image)
