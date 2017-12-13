bc0d113ac281772c95b2bae0693f088ebf304942
jeongjoonsup <penny4860@gmail.com>
2017-12-13 19:45:24 +0900

    YoloLoss class extraction

--- yolo/frontend.py
+++ yolo/frontend.py
@@ -25,7 +25,6 @@ class YOLO(object):
         self.labels   = list(labels)
         self.nb_class = len(self.labels)
         self.nb_box   = 5
-        self.class_wt = np.ones(self.nb_class, dtype='float32')
         self.anchors  = anchors
         self.max_box_per_image = max_box_per_image
 
@@ -67,7 +66,143 @@ class YOLO(object):
         _init_layer(model.layers[-4])
         return model
 
-    def _custom_loss(self, y_true, y_pred):
+    def load_weights(self, weight_path):
+        self.model.load_weights(weight_path)
+
+    def predict(self, image):
+        """
+        # Args
+            image : 3d-array
+        
+        # Returns
+            boxes : list of BoundBox instance
+        """
+        image = cv2.resize(image, (self.input_size, self.input_size))
+        image = self.feature_extractor.normalize(image)
+
+        input_image = image[:,:,::-1]
+        input_image = np.expand_dims(input_image, 0)
+        dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
+
+        # (13,13,5,6)
+        netout = self.model.predict([input_image, dummy_array])[0]
+        yolo_decoder = YoloDecoder(self.anchors)
+        boxes = yolo_decoder.run(netout)
+        return boxes
+
+    def train(self, train_imgs,     # the list of images to train the model
+                    valid_imgs,     # the list of images used to validate the model
+                    train_times,    # the number of time to repeat the training set, often used for small datasets
+                    valid_times,    # the number of times to repeat the validation set, often used for small datasets
+                    nb_epoch,       # number of epoches
+                    learning_rate,  # the learning rate
+                    batch_size,     # the size of the batch
+                    warmup_epochs,  # number of initial batches to let the model familiarize with the new dataset
+                    saved_weights_name='best_weights.h5',
+                    debug=False):     
+
+        self.batch_size = batch_size
+        self.debug = debug
+
+        if warmup_epochs > 0: nb_epoch = warmup_epochs # if it's warmup stage, don't train more than warmup_epochs
+
+        ############################################
+        # Compile the model
+        ############################################
+
+        optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        
+        warmup_bs  = warmup_epochs * (train_times*(len(train_imgs)/batch_size+1) + valid_times*(len(valid_imgs)/batch_size+1))
+        yolo_loss = YoloLoss(self.grid_w,
+                             self.grid_h,
+                             self.batch_size,
+                             self.anchors,
+                             self.nb_box,
+                             self.nb_class,
+                             warmup_bs,
+                             self.true_boxes)
+        self.model.compile(loss=yolo_loss.custom_loss, optimizer=optimizer)
+
+        ############################################
+        # Make train and validation generators
+        ############################################
+        train_batch, valid_batch = self._create_batch_generator(train_imgs, valid_imgs)
+
+        ############################################
+        # Start the training process
+        ############################################        
+        self.model.fit_generator(generator        = train_batch, 
+                                 steps_per_epoch  = len(train_batch) * train_times, 
+                                 epochs           = nb_epoch, 
+                                 verbose          = 1,
+                                 validation_data  = valid_batch,
+                                 validation_steps = len(valid_batch) * valid_times,
+                                 callbacks        = self._create_callbacks(saved_weights_name), 
+                                 workers          = 3,
+                                 max_queue_size   = 8)
+
+    def _create_batch_generator(self, train_imgs, valid_imgs):
+        generator_config = {
+            'IMAGE_H'         : self.input_size, 
+            'IMAGE_W'         : self.input_size,
+            'GRID_H'          : self.grid_h,  
+            'GRID_W'          : self.grid_w,
+            'BOX'             : self.nb_box,
+            'LABELS'          : self.labels,
+            'CLASS'           : len(self.labels),
+            'ANCHORS'         : self.anchors,
+            'BATCH_SIZE'      : self.batch_size,
+            'TRUE_BOX_BUFFER' : self.max_box_per_image,
+        }    
+
+        train_batch = BatchGenerator(train_imgs, 
+                                     generator_config, 
+                                     norm=self.feature_extractor.normalize)
+        valid_batch = BatchGenerator(valid_imgs, 
+                                     generator_config, 
+                                     norm=self.feature_extractor.normalize,
+                                     jitter=False)
+        return train_batch, valid_batch
+
+    def _create_callbacks(self, saved_weights_name):
+        # Make a few callbacks
+        early_stop = EarlyStopping(monitor='val_loss', 
+                           min_delta=0.001, 
+                           patience=3, 
+                           mode='min', 
+                           verbose=1)
+        checkpoint = ModelCheckpoint(saved_weights_name, 
+                                     monitor='val_loss', 
+                                     verbose=1, 
+                                     save_best_only=True, 
+                                     mode='min', 
+                                     period=1)
+        tb_counter  = len([log for log in os.listdir(os.path.expanduser('~/logs/')) if 'yolo' in log]) + 1
+        tensorboard = TensorBoard(log_dir=os.path.expanduser('~/logs/') + 'yolo' + '_' + str(tb_counter), 
+                                  histogram_freq=0, 
+                                  write_graph=True, 
+                                  write_images=False)
+        callbacks = [early_stop, checkpoint, tensorboard]
+        return callbacks
+        
+
+class YoloLoss(object):
+    
+    def __init__(self, grid_w, grid_h, batch_size, anchors, nb_box, nb_class, warmup_bs, true_boxes):
+        self.grid_w = grid_w
+        self.grid_h = grid_h
+        self.batch_size = batch_size
+        self.anchors = anchors
+        self.nb_box = nb_box
+        self.nb_class = nb_class
+        self.warmup_bs = warmup_bs
+        self.true_boxes = true_boxes
+        self.object_scale    = 5.0
+        self.no_object_scale = 1.0
+        self.coord_scale     = 1.0
+        self.class_scale     = 1.0
+    
+    def custom_loss(self, y_true, y_pred):
         mask_shape = tf.shape(y_true)[:4]
         
         cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(self.grid_w), [self.grid_h]), (1, self.grid_h, self.grid_w, 1, 1)))
@@ -171,7 +306,8 @@ class YOLO(object):
         conf_mask = conf_mask + y_true[..., 4] * self.object_scale
         
         ### class mask: simply the position of the ground truth boxes (the predictors)
-        class_mask = y_true[..., 4] * tf.gather(self.class_wt, true_box_class) * self.class_scale       
+        class_wt = np.ones(self.nb_class, dtype='float32')
+        class_mask = y_true[..., 4] * tf.gather(class_wt, true_box_class) * self.class_scale       
         
         """
         Warm-up training
@@ -202,142 +338,27 @@ class YOLO(object):
         
         loss = loss_xy + loss_wh + loss_conf + loss_class
         
-        if self.debug:
-            nb_true_box = tf.reduce_sum(y_true[..., 4])
-            nb_pred_box = tf.reduce_sum(tf.to_float(true_box_conf > 0.5) * tf.to_float(pred_box_conf > 0.3))
-            
-            current_recall = nb_pred_box/(nb_true_box + 1e-6)
-            total_recall = tf.assign_add(total_recall, current_recall) 
-
-            loss = tf.Print(loss, [tf.zeros((1))], message='Dummy Line \t', summarize=1000)
-            loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
-            loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
-            loss = tf.Print(loss, [loss_conf], message='Loss Conf \t', summarize=1000)
-            loss = tf.Print(loss, [loss_class], message='Loss Class \t', summarize=1000)
-            loss = tf.Print(loss, [loss], message='Total Loss \t', summarize=1000)
-            loss = tf.Print(loss, [current_recall], message='Current Recall \t', summarize=1000)
-            loss = tf.Print(loss, [total_recall/seen], message='Average Recall \t', summarize=1000)
+#         if self.debug:
+#             nb_true_box = tf.reduce_sum(y_true[..., 4])
+#             nb_pred_box = tf.reduce_sum(tf.to_float(true_box_conf > 0.5) * tf.to_float(pred_box_conf > 0.3))
+#             
+#             current_recall = nb_pred_box/(nb_true_box + 1e-6)
+#             total_recall = tf.assign_add(total_recall, current_recall) 
+# 
+#             loss = tf.Print(loss, [tf.zeros((1))], message='Dummy Line \t', summarize=1000)
+#             loss = tf.Print(loss, [loss_xy], message='Loss XY \t', summarize=1000)
+#             loss = tf.Print(loss, [loss_wh], message='Loss WH \t', summarize=1000)
+#             loss = tf.Print(loss, [loss_conf], message='Loss Conf \t', summarize=1000)
+#             loss = tf.Print(loss, [loss_class], message='Loss Class \t', summarize=1000)
+#             loss = tf.Print(loss, [loss], message='Total Loss \t', summarize=1000)
+#             loss = tf.Print(loss, [current_recall], message='Current Recall \t', summarize=1000)
+#             loss = tf.Print(loss, [total_recall/seen], message='Average Recall \t', summarize=1000)
         
         return loss
-
-    def load_weights(self, weight_path):
-        self.model.load_weights(weight_path)
-
-    def predict(self, image):
-        """
-        # Args
-            image : 3d-array
         
-        # Returns
-            boxes : list of BoundBox instance
-        """
-        image = cv2.resize(image, (self.input_size, self.input_size))
-        image = self.feature_extractor.normalize(image)
-
-        input_image = image[:,:,::-1]
-        input_image = np.expand_dims(input_image, 0)
-        dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
-
-        # (13,13,5,6)
-        netout = self.model.predict([input_image, dummy_array])[0]
-        yolo_decoder = YoloDecoder(self.anchors)
-        boxes = yolo_decoder.run(netout)
-        return boxes
-
-    def train(self, train_imgs,     # the list of images to train the model
-                    valid_imgs,     # the list of images used to validate the model
-                    train_times,    # the number of time to repeat the training set, often used for small datasets
-                    valid_times,    # the number of times to repeat the validation set, often used for small datasets
-                    nb_epoch,       # number of epoches
-                    learning_rate,  # the learning rate
-                    batch_size,     # the size of the batch
-                    warmup_epochs,  # number of initial batches to let the model familiarize with the new dataset
-                    object_scale,
-                    no_object_scale,
-                    coord_scale,
-                    class_scale,
-                    saved_weights_name='best_weights.h5',
-                    debug=False):     
-
-        self.batch_size = batch_size
-        self.warmup_bs  = warmup_epochs * (train_times*(len(train_imgs)/batch_size+1) + valid_times*(len(valid_imgs)/batch_size+1))
-
-        self.object_scale    = object_scale
-        self.no_object_scale = no_object_scale
-        self.coord_scale     = coord_scale
-        self.class_scale     = class_scale
-
-        self.debug = debug
-
-        if warmup_epochs > 0: nb_epoch = warmup_epochs # if it's warmup stage, don't train more than warmup_epochs
-
-        ############################################
-        # Compile the model
-        ############################################
-
-        optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        self.model.compile(loss=self._custom_loss, optimizer=optimizer)
-
-        ############################################
-        # Make train and validation generators
-        ############################################
-        train_batch, valid_batch = self._create_batch_generator(train_imgs, valid_imgs)
-
-        ############################################
-        # Start the training process
-        ############################################        
-        self.model.fit_generator(generator        = train_batch, 
-                                 steps_per_epoch  = len(train_batch) * train_times, 
-                                 epochs           = nb_epoch, 
-                                 verbose          = 1,
-                                 validation_data  = valid_batch,
-                                 validation_steps = len(valid_batch) * valid_times,
-                                 callbacks        = self._create_callbacks(saved_weights_name), 
-                                 workers          = 3,
-                                 max_queue_size   = 8)
-
-    def _create_batch_generator(self, train_imgs, valid_imgs):
-        generator_config = {
-            'IMAGE_H'         : self.input_size, 
-            'IMAGE_W'         : self.input_size,
-            'GRID_H'          : self.grid_h,  
-            'GRID_W'          : self.grid_w,
-            'BOX'             : self.nb_box,
-            'LABELS'          : self.labels,
-            'CLASS'           : len(self.labels),
-            'ANCHORS'         : self.anchors,
-            'BATCH_SIZE'      : self.batch_size,
-            'TRUE_BOX_BUFFER' : self.max_box_per_image,
-        }    
-
-        train_batch = BatchGenerator(train_imgs, 
-                                     generator_config, 
-                                     norm=self.feature_extractor.normalize)
-        valid_batch = BatchGenerator(valid_imgs, 
-                                     generator_config, 
-                                     norm=self.feature_extractor.normalize,
-                                     jitter=False)
-        return train_batch, valid_batch
-
-    def _create_callbacks(self, saved_weights_name):
-        # Make a few callbacks
-        early_stop = EarlyStopping(monitor='val_loss', 
-                           min_delta=0.001, 
-                           patience=3, 
-                           mode='min', 
-                           verbose=1)
-        checkpoint = ModelCheckpoint(saved_weights_name, 
-                                     monitor='val_loss', 
-                                     verbose=1, 
-                                     save_best_only=True, 
-                                     mode='min', 
-                                     period=1)
-        tb_counter  = len([log for log in os.listdir(os.path.expanduser('~/logs/')) if 'yolo' in log]) + 1
-        tensorboard = TensorBoard(log_dir=os.path.expanduser('~/logs/') + 'yolo' + '_' + str(tb_counter), 
-                                  histogram_freq=0, 
-                                  write_graph=True, 
-                                  write_images=False)
-        callbacks = [early_stop, checkpoint, tensorboard]
-        return callbacks
         
+        
+    
+
+
 
