4381a8d9faff27539642fd026de1855a23ca5c79
jeongjoonsup <penny4860@gmail.com>
2017-12-14 21:31:12 +0900

    YoloTrainer class extraction

--- yolo/frontend.py
+++ yolo/frontend.py
@@ -101,59 +101,6 @@ class YOLO(object):
                     saved_weights_name='best_weights.h5',
                     debug=False):     
 
-        self.batch_size = batch_size
-        self.debug = debug
-
-        if warmup_epochs > 0: nb_epoch = warmup_epochs # if it's warmup stage, don't train more than warmup_epochs
-
-        ############################################
-        # Compile the model
-        ############################################
-
-        
-        warmup_bs  = warmup_epochs * (train_times*(len(train_imgs)/batch_size+1) + valid_times*(len(valid_imgs)/batch_size+1))
-
-        optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        yolo_loss = YoloLoss(self.grid_w,
-                             self.grid_h,
-                             self.batch_size,
-                             self.anchors,
-                             self.nb_box,
-                             self.nb_class,
-                             warmup_bs,
-                             self.true_boxes)
-        self.model.compile(loss=yolo_loss.custom_loss, optimizer=optimizer)
-
-        yolo_trainer = YoloTrainer()
-        yolo_trainer.run()
-
-
-class YoloTrainer(object):
-    
-    def __init__(self):
-        pass
-    
-    def train(self, train_imgs,     # the list of images to train the model
-                    valid_imgs,     # the list of images used to validate the model
-                    train_times,    # the number of time to repeat the training set, often used for small datasets
-                    valid_times,    # the number of times to repeat the validation set, often used for small datasets
-                    nb_epoch,       # number of epoches
-                    learning_rate,  # the learning rate
-                    batch_size,     # the size of the batch
-                    warmup_epochs,  # number of initial batches to let the model familiarize with the new dataset
-                    saved_weights_name='best_weights.h5',
-                    debug=False):
-
-        self.debug = debug
-
-        if warmup_epochs > 0: nb_epoch = warmup_epochs # if it's warmup stage, don't train more than warmup_epochs
-
-        ############################################
-        # Compile the model
-        ############################################
-
-        optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        
         warmup_bs  = warmup_epochs * (train_times*(len(train_imgs)/batch_size+1) + valid_times*(len(valid_imgs)/batch_size+1))
         yolo_loss = YoloLoss(self.grid_w,
                              self.grid_h,
@@ -163,27 +110,7 @@ class YoloTrainer(object):
                              self.nb_class,
                              warmup_bs,
                              self.true_boxes)
-        self.model.compile(loss=yolo_loss.custom_loss, optimizer=optimizer)
-
-        ############################################
-        # Make train and validation generators
-        ############################################
-        train_batch, valid_batch = self._create_batch_generator(train_imgs, valid_imgs, batch_size)
-
-        ############################################
-        # Start the training process
-        ############################################        
-        self.model.fit_generator(generator        = train_batch, 
-                                 steps_per_epoch  = len(train_batch) * train_times, 
-                                 epochs           = nb_epoch, 
-                                 verbose          = 1,
-                                 validation_data  = valid_batch,
-                                 validation_steps = len(valid_batch) * valid_times,
-                                 callbacks        = self._create_callbacks(saved_weights_name), 
-                                 workers          = 3,
-                                 max_queue_size   = 8)
-
-    def _create_batch_generator(self, train_imgs, valid_imgs, batch_size):
+        
         generator_config = {
             'IMAGE_H'         : self.input_size, 
             'IMAGE_W'         : self.input_size,
@@ -197,12 +124,63 @@ class YoloTrainer(object):
             'TRUE_BOX_BUFFER' : self.max_box_per_image,
         }    
 
+        yolo_trainer = YoloTrainer(self.model, yolo_loss.custom_loss, self.feature_extractor.normalize, generator_config)
+        yolo_trainer.train(train_imgs,
+                           valid_imgs,
+                           train_times,
+                           valid_times,
+                           nb_epoch,
+                           learning_rate,
+                           warmup_epochs,
+                           saved_weights_name)
+
+
+class YoloTrainer(object):
+    
+    def __init__(self, model, loss_func, normalize_func, generator_config):
+        self.model = model
+        self.loss_func = loss_func
+        self.normalize_func = normalize_func
+        self.generator_config = generator_config
+        
+    def train(self, train_imgs,     # the list of images to train the model
+                    valid_imgs,     # the list of images used to validate the model
+                    train_times,    # the number of time to repeat the training set, often used for small datasets
+                    valid_times,    # the number of times to repeat the validation set, often used for small datasets
+                    nb_epoch,       # number of epoches
+                    learning_rate,  # the learning rate
+                    warmup_epochs,  # number of initial batches to let the model familiarize with the new dataset
+                    saved_weights_name='best_weights.h5'):
+
+        if warmup_epochs > 0: nb_epoch = warmup_epochs # if it's warmup stage, don't train more than warmup_epochs
+
+        # 1. create optimizer
+        optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        
+        # 2. create loss function
+        self.model.compile(loss=self.loss_func, optimizer=optimizer)
+
+        # 3. batch generator
+        train_batch, valid_batch = self._create_batch_generator(train_imgs, valid_imgs)
+
+        # 4. training
+        self.model.fit_generator(generator        = train_batch, 
+                                 steps_per_epoch  = len(train_batch) * train_times, 
+                                 epochs           = nb_epoch, 
+                                 verbose          = 1,
+                                 validation_data  = valid_batch,
+                                 validation_steps = len(valid_batch) * valid_times,
+                                 callbacks        = self._create_callbacks(saved_weights_name), 
+                                 workers          = 3,
+                                 max_queue_size   = 8)
+
+    def _create_batch_generator(self, train_imgs, valid_imgs):
         train_batch = BatchGenerator(train_imgs, 
-                                     generator_config, 
-                                     norm=self.feature_extractor.normalize)
+                                     self.generator_config, 
+                                     norm=self.normalize_func)
         valid_batch = BatchGenerator(valid_imgs, 
-                                     generator_config, 
-                                     norm=self.feature_extractor.normalize,
+                                     self.generator_config, 
+                                     norm=self.normalize_func,
                                      jitter=False)
         return train_batch, valid_batch
 
@@ -228,7 +206,8 @@ class YoloTrainer(object):
         return callbacks
     
     
-
+    
+    
 
 
 
