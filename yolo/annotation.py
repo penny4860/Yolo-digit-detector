9d211ae1b84bdc69d9c05adb5a483af7e35b8fab
jeongjoonsup <penny4860@gmail.com>
2017-12-15 19:33:56 +0900

    test code separation

--- yolo/annotation.py
+++ yolo/annotation.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import xml.etree.ElementTree as ET
-
-
 import numpy as np
 from xml.etree.ElementTree import parse
 
@@ -150,50 +147,4 @@ def parse_annotation(ann_dir, img_dir, labels=[]):
             all_imgs += [img]
                         
     return all_imgs, seen_labels
-
-
-
-import pytest
-SAMPLE_DIRECTORY = "..//sample"
-
-@pytest.fixture(scope='function')
-def setup_inputs(request):
-    annotation_dir = os.path.join(SAMPLE_DIRECTORY, "raccoon_train_annotations//")
-    image_dir = os.path.join(SAMPLE_DIRECTORY, "raccoon_train_imgs//")
-    return annotation_dir, image_dir
-
-@pytest.fixture(scope='function')
-def setup_expected_outputs(request, setup_inputs):
-    _, image_dir = setup_inputs
-    
-    images = [{'object': [{'name': 'raccoon', 'xmin': 81, 'ymin': 88, 'xmax': 522, 'ymax': 408}],
-               'filename': os.path.join(image_dir, 'raccoon-1.jpg'),
-               'width': 650,
-               'height': 417}]
-    labels = {'raccoon': 1}
-    return images, labels
-
-
-def test_parse_annotation(setup_inputs, setup_expected_outputs):
-    
-    # Given
-    annotation_dir, image_dir = setup_inputs
-    
-    # When
-    all_imgs, seen_labels = parse_annotation(annotation_dir, image_dir)
-    
-    images, labels = setup_expected_outputs
-    
-    assert all_imgs == images
-    assert seen_labels == labels
-    
-
-if __name__ == '__main__':
-    pytest.main([__file__, "-v", "-s"])
-    
-    
-    
-    
-    
-
 
