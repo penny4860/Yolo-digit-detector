4cc3eee457509d90dbcb176024b7df0f7dd0a491
jeongjoonsup <penny4860@gmail.com>
2017-12-16 18:09:53 +0900

    nit : 모듈 정리

--- yolo/augment.py
+++ yolo/augment.py
@@ -1,71 +1,42 @@
-
+# -*- coding: utf-8 -*-
 from imgaug import augmenters as iaa
-import copy
 import cv2
 import numpy as np
-from astroid.protocols import objects
 
 
-def _create_augment_pipeline():
+class ImgAugment(object):
     
-        ### augmentors by https://github.com/aleju/imgaug
-        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+    def __init__(self):
+        pass
 
-        # Define our sequence of augmentation steps that will be applied to every image
-        # All augmenters with per_channel=0.5 will sample one value _per image_
-        # in 50% of all cases. In all other cases they will sample new values
-        # _per channel_.
-        aug_pipe = iaa.Sequential(
-            [
-                # apply the following augmenters to most images
-                #iaa.Fliplr(0.5), # horizontally flip 50% of all images
-                #iaa.Flipud(0.2), # vertically flip 20% of all images
-                #sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
-                sometimes(iaa.Affine(
-                    #scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                    #translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-                    #rotate=(-5, 5), # rotate by -45 to +45 degrees
-                    #shear=(-5, 5), # shear by -16 to +16 degrees
-                    #order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-                    #cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-                    #mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-                )),
-                # execute 0 to 5 of the following (less important) augmenters per image
-                # don't execute all of them, as that would often be way too strong
-                iaa.SomeOf((0, 5),
-                    [
-                        #sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-                        iaa.OneOf([
-                            iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-                            iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                            iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
-                        ]),
-                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
-                        #iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-                        # search either for all edges or for directed edges
-                        #sometimes(iaa.OneOf([
-                        #    iaa.EdgeDetect(alpha=(0, 0.7)),
-                        #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
-                        #])),
-                        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
-                        iaa.OneOf([
-                            iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
-                            #iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                        ]),
-                        #iaa.Invert(0.05, per_channel=True), # invert color channels
-                        iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
-                        iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
-                        iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
-                        #iaa.Grayscale(alpha=(0.0, 1.0)),
-                        #sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-                        #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))) # sometimes move parts of the image around
-                    ],
-                    random_order=True
-                )
-            ],
-            random_order=True
-        )
-        return aug_pipe
+    def run(self, img_file,
+            boxes,
+            desired_w,
+            desired_h,
+            jitter):
+        """
+        # Args
+            img_file : str
+            boxes : array, shape of (N, 4)
+                (x1,y1,x2,y2)-ordered
+            desired_w, desired_h: int
+            jitter : bool
+        
+        # Returns
+            image : ndarray, shape of (h, w, 3)
+                BRG-ordered image array
+            boxes : ndarray, shape of (N, 4)
+        """
+        # 1. read image file
+        image = cv2.imread(img_file)
+
+        # 2. make jitter on image        
+        if jitter:
+            image, boxes = make_jitter_on_image(image, boxes)
+
+        # 3. resize image            
+        image, boxes = resize_image(image, boxes, desired_w, desired_h)
+        return image, boxes
 
 
 def make_jitter_on_image(image, boxes):
@@ -137,39 +108,66 @@ def resize_image(image, boxes, desired_w, desired_h):
     return image, np.array(new_boxes)
 
 
-class ImgAugment(object):
+def _create_augment_pipeline():
     
-    def __init__(self):
-        pass
+    ### augmentors by https://github.com/aleju/imgaug
+    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
-    def run(self, img_file,
-            boxes,
-            desired_w,
-            desired_h,
-            jitter):
-        """
-        # Args
-            img_file : str
-            boxes : array, shape of (N, 4)
-                (x1,y1,x2,y2)-ordered
-            desired_w, desired_h: int
-            jitter : bool
-        
-        # Returns
-            image : ndarray, shape of (h, w, 3)
-                BRG-ordered image array
-            boxes : ndarray, shape of (N, 4)
-        """
-        # 1. read image file
-        image = cv2.imread(img_file)
-
-        # 2. make jitter on image        
-        if jitter:
-            image, boxes = make_jitter_on_image(image, boxes)
-
-        # 3. resize image            
-        image, boxes = resize_image(image, boxes, desired_w, desired_h)
-        return image, boxes
+    # Define our sequence of augmentation steps that will be applied to every image
+    # All augmenters with per_channel=0.5 will sample one value _per image_
+    # in 50% of all cases. In all other cases they will sample new values
+    # _per channel_.
+    aug_pipe = iaa.Sequential(
+        [
+            # apply the following augmenters to most images
+            #iaa.Fliplr(0.5), # horizontally flip 50% of all images
+            #iaa.Flipud(0.2), # vertically flip 20% of all images
+            #sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
+            sometimes(iaa.Affine(
+                #scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
+                #translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+                #rotate=(-5, 5), # rotate by -45 to +45 degrees
+                #shear=(-5, 5), # shear by -16 to +16 degrees
+                #order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+                #cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+                #mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+            )),
+            # execute 0 to 5 of the following (less important) augmenters per image
+            # don't execute all of them, as that would often be way too strong
+            iaa.SomeOf((0, 5),
+                [
+                    #sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                    iaa.OneOf([
+                        iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                        iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                        iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                    ]),
+                    iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                    #iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                    # search either for all edges or for directed edges
+                    #sometimes(iaa.OneOf([
+                    #    iaa.EdgeDetect(alpha=(0, 0.7)),
+                    #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
+                    #])),
+                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
+                    iaa.OneOf([
+                        iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                        #iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                    ]),
+                    #iaa.Invert(0.05, per_channel=True), # invert color channels
+                    iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+                    iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
+                    iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
+                    #iaa.Grayscale(alpha=(0.0, 1.0)),
+                    #sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                    #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))) # sometimes move parts of the image around
+                ],
+                random_order=True
+            )
+        ],
+        random_order=True
+    )
+    return aug_pipe
 
 
 if __name__ == '__main__':
