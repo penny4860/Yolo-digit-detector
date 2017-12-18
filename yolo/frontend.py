7e50f4272eebc2d60b3e8abb1ad8758bfe496a88
jeongjoonsup <penny4860@gmail.com>
2017-12-18 23:56:13 +0900

    test_predict() passed

--- yolo/frontend.py
+++ yolo/frontend.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import cv2
-
-from keras.models import Model
-from keras.layers import Reshape, Conv2D, Input, Lambda
+from keras.layers import Input
 
 from yolo.decoder import YoloDecoder
 from yolo.loss import YoloLoss
@@ -15,7 +11,7 @@ from yolo.preprocessing import GeneratorConfig
 # client : predict.py
 # client : train.py
 class YOLO(object):
-    def __init__(self, feature_extractor,
+    def __init__(self, network,
                        labels, 
                        max_box_per_image,
                        anchors):
@@ -23,8 +19,8 @@ class YOLO(object):
         # Args
             feature_extractor : BaseFeatureExtractor instance
         """
-
-        self.input_size = feature_extractor.get_input_size()
+        self.network = network
+        # self.input_size = feature_extractor.get_input_size()
         
         self.labels   = list(labels)
         self.nb_class = len(self.labels)
@@ -32,46 +28,14 @@ class YOLO(object):
         self.anchors  = anchors
         self.max_box_per_image = max_box_per_image
 
-        # create feature extractor
-        self.feature_extractor = feature_extractor
-        self.grid_h, self.grid_w = self.feature_extractor.get_output_shape()        
-
         # truth tensor
         self.true_boxes = Input(shape=(1, 1, 1, self.max_box_per_image , 4))
 
-        self.model = self._create_network()
-        
         # print a summary of the whole model
-        self.model.summary()
-
-    def _create_network(self):
-        def _init_layer(layer):
-            weights = layer.get_weights()
-    
-            new_kernel = np.random.normal(size=weights[0].shape)/(self.grid_h*self.grid_w)
-            new_bias   = np.random.normal(size=weights[1].shape)/(self.grid_h*self.grid_w)
-    
-            layer.set_weights([new_kernel, new_bias])
-        
-        # make the feature extractor layers
-        input_tensor = Input(shape=(self.input_size, self.input_size, 3))
-        features = self.feature_extractor.extract(input_tensor)      
-
-        # make the object detection layer
-        output_tensor = Conv2D(self.nb_box * (4 + 1 + self.nb_class), 
-                        (1,1), strides=(1,1), 
-                        padding='same', 
-                        name='conv_23', 
-                        kernel_initializer='lecun_normal')(features)
-        output_tensor = Reshape((self.grid_h, self.grid_w, self.nb_box, 4 + 1 + self.nb_class))(output_tensor)
-        output_tensor = Lambda(lambda args: args[0])([output_tensor, self.true_boxes])
-
-        model = Model([input_tensor, self.true_boxes], output_tensor)
-        _init_layer(model.layers[-4])
-        return model
+        self.network.model.summary()
 
     def load_weights(self, weight_path):
-        self.model.load_weights(weight_path)
+        self.network.load_weights(weight_path)
 
     def predict(self, image):
         """
@@ -81,15 +45,7 @@ class YOLO(object):
         # Returns
             boxes : list of BoundBox instance
         """
-        image = cv2.resize(image, (self.input_size, self.input_size))
-        image = self.feature_extractor.normalize(image)
-
-        input_image = image[:,:,::-1]
-        input_image = np.expand_dims(input_image, 0)
-        dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
-
-        # (13,13,5,6)
-        netout = self.model.predict([input_image, dummy_array])[0]
+        netout = self.network.forward(image)
         yolo_decoder = YoloDecoder(self.anchors)
         boxes = yolo_decoder.run(netout)
         return boxes
@@ -122,8 +78,9 @@ class YOLO(object):
                                            batch_size,
                                            self.max_box_per_image,
                                            self.anchors)
-
-        yolo_trainer = YoloTrainer(self.model, yolo_loss.custom_loss, self.feature_extractor.normalize, generator_config)
+        
+        # Todo : self.model.model 정리
+        yolo_trainer = YoloTrainer(self.network.model, yolo_loss.custom_loss, self.feature_extractor.normalize, generator_config)
         yolo_trainer.train(train_imgs,
                            valid_imgs,
                            train_times,
--- yolo/network.py
+++ yolo/network.py
@@ -5,26 +5,32 @@ import numpy as np
 
 from yolo.backend import create_feature_extractor
 
+
+nb_box = 5
+
 class YoloNetwork(object):
     
-    def __init__(self, architecture, input_size, nb_box, nb_classes, max_box_per_image=10):
-        self._feature_extractor = create_feature_extractor(architecture, input_size)
+    def __init__(self,
+                 architecture,
+                 input_size,
+                 nb_classes,
+                 max_box_per_image=10):
     
         def _init_layer(layer):
             weights = layer.get_weights()
     
-            new_kernel = np.random.normal(size=weights[0].shape)/(grid_h*grid_w)
-            new_bias   = np.random.normal(size=weights[1].shape)/(grid_h*grid_w)
+            new_kernel = np.random.normal(size=weights[0].shape)/(self._grid_h*self._grid_w)
+            new_bias   = np.random.normal(size=weights[1].shape)/(self._grid_h*self._grid_w)
     
             layer.set_weights([new_kernel, new_bias])
         
         # 1. create feature extractor
-        feature_extractor = create_feature_extractor(architecture, input_size)
+        self._feature_extractor = create_feature_extractor(architecture, input_size)
         
         # 2. create full network
         input_tensor = Input(shape=(input_size, input_size, 3))
-        features = feature_extractor.extract(input_tensor)
-        grid_h, grid_w = feature_extractor.get_output_shape()
+        features = self._feature_extractor.extract(input_tensor)
+        self._grid_h, self._grid_w = self._feature_extractor.get_output_shape()
         
         # truth tensor
         true_boxes = Input(shape=(1, 1, 1, max_box_per_image , 4))
@@ -35,12 +41,30 @@ class YoloNetwork(object):
                         padding='same', 
                         name='conv_23', 
                         kernel_initializer='lecun_normal')(features)
-        output_tensor = Reshape((grid_h, grid_w, nb_box, 4 + 1 + nb_classes))(output_tensor)
+        output_tensor = Reshape((self._grid_h, self._grid_w, nb_box, 4 + 1 + nb_classes))(output_tensor)
         output_tensor = Lambda(lambda args: args[0])([output_tensor, true_boxes])
     
         model = Model([input_tensor, true_boxes], output_tensor)
         _init_layer(model.layers[-4])
+
         self.model = model
-    
-    
+        self.input_size = input_size
+        self.max_box_per_image = max_box_per_image
+
+    def load_weights(self, weight_path):
+        self.model.load_weights(weight_path)
+        
+    def forward(self, image):
+        import cv2
+        image = cv2.resize(image, (self.input_size, self.input_size))
+        image = self._feature_extractor.normalize(image)
+
+        input_image = image[:,:,::-1]
+        input_image = np.expand_dims(input_image, 0)
+        dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
+
+        # (13,13,5,6)
+        netout = self.model.predict([input_image, dummy_array])[0]
+        return netout
+
     
