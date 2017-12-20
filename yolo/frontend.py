68bca0ca3a043168997c71dff85c848550b01a16
jeongjoonsup <penny4860@gmail.com>
2017-12-20 21:06:29 +0900

    YoloNetwork 를 YOLO constructor 에서 생성

--- yolo/frontend.py
+++ yolo/frontend.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from yolo.decoder import YoloDecoder
-from yolo.loss import YoloLoss
-from yolo.trainer import YoloTrainer
-from yolo.batch_gen import GeneratorConfig
+from yolo.network import YoloNetwork
 
 # create_feature_extractor(architecture, input_size)
 # client : predict.py
 # client : train.py
 class YOLO(object):
     def __init__(self,
-                 network,
-                 anchors):
+                 architecture,
+                 input_size = 416,
+                 n_classes = 1,
+                 max_box_per_image = 10,
+                 anchors = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]):
         """
         # Args
             feature_extractor : BaseFeatureExtractor instance
         """
-        self._yolo_network = network
+        self._yolo_network = YoloNetwork(architecture, input_size, n_classes, max_box_per_image, anchors)
         self._yolo_decoder = YoloDecoder(anchors)
 
     def load_weights(self, weight_path):
