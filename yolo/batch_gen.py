56fd9ddafaf718ac95fe468a284137b90cf8f5bc
jeongjoonsup <penny4860@gmail.com>
2017-12-25 20:39:36 +0900

    nit

--- yolo/batch_gen.py
+++ yolo/batch_gen.py
@@ -6,77 +6,6 @@ from keras.utils import Sequence
 from yolo.box import to_centroid, to_normalize, create_anchor_boxes, find_match_box
 
 
-
-class _TrueBoxGen(object):
-    def __init__(self):
-        pass
-
-    def run(self, norm_boxes, max_box_per_image):
-        """
-        # Args
-            labels : list of integers
-            
-            y_shape : tuple
-                (grid_size, grid_size, nb_boxes, 4+1+nb_classes)
-            b_shape : tuple
-                (1, 1, 1, max_box_per_image, 4)
-        """
-        
-        # construct output from object's x, y, w, h
-        true_box_index = 0
-        true_boxes = np.zeros((max_box_per_image, 4))
-        
-        # loop over objects in one image
-        for norm_box in norm_boxes:
-            # assign the true box to b_batch
-            true_boxes[true_box_index, :] = norm_box
-            true_box_index += 1
-            true_box_index = true_box_index % max_box_per_image
-        return true_boxes
-
-
-class _NetoutGen(object):
-    def __init__(self, anchors=[0.57273, 0.677385,
-                                1.87446, 2.06253,
-                                3.33843, 5.47434,
-                                7.88282, 3.52778,
-                                9.77052, 9.16828]):
-        self.anchors = create_anchor_boxes(anchors)
-
-    def run(self, norm_boxes, labels, y_shape):
-        """
-        # Args
-            labels : list of integers
-            
-            y_shape : tuple
-                (grid_size, grid_size, nb_boxes, 4+1+nb_classes)
-            b_shape : tuple
-                (1, 1, 1, max_box_per_image, 4)
-        """
-        y = np.zeros(y_shape)
-        
-        # loop over objects in one image
-        for norm_box, label in zip(norm_boxes, labels):
-            best_anchor = self._find_anchor_idx(norm_box)
-
-            # assign ground truth x, y, w, h, confidence and class probs to y_batch
-            y += self._generate_y(best_anchor, label, norm_box, y_shape)
-        return y
-
-    def _find_anchor_idx(self, norm_box):
-        _, _, center_w, center_h = norm_box
-        shifted_box = np.array([0, 0, center_w, center_h])
-        return find_match_box(shifted_box, self.anchors)
-    
-    def _generate_y(self, best_anchor, obj_indx, box, y_shape):
-        y = np.zeros(y_shape)
-        grid_x, grid_y, _, _ = box.astype(int)
-        y[grid_y, grid_x, best_anchor, 0:4] = box
-        y[grid_y, grid_x, best_anchor, 4  ] = 1.
-        y[grid_y, grid_x, best_anchor, 5+obj_indx] = 1
-        return y
-
-
 class BatchGenerator(Sequence):
     def __init__(self, annotations, 
                        input_size=416,
@@ -155,3 +84,73 @@ class BatchGenerator(Sequence):
     def on_epoch_end(self):
         self.annotations.shuffle()
         self.counter = 0
+
+
+class _TrueBoxGen(object):
+    def __init__(self):
+        pass
+
+    def run(self, norm_boxes, max_box_per_image):
+        """
+        # Args
+            labels : list of integers
+            
+            y_shape : tuple
+                (grid_size, grid_size, nb_boxes, 4+1+nb_classes)
+            b_shape : tuple
+                (1, 1, 1, max_box_per_image, 4)
+        """
+        
+        # construct output from object's x, y, w, h
+        true_box_index = 0
+        true_boxes = np.zeros((max_box_per_image, 4))
+        
+        # loop over objects in one image
+        for norm_box in norm_boxes:
+            # assign the true box to b_batch
+            true_boxes[true_box_index, :] = norm_box
+            true_box_index += 1
+            true_box_index = true_box_index % max_box_per_image
+        return true_boxes
+
+
+class _NetoutGen(object):
+    def __init__(self, anchors=[0.57273, 0.677385,
+                                1.87446, 2.06253,
+                                3.33843, 5.47434,
+                                7.88282, 3.52778,
+                                9.77052, 9.16828]):
+        self.anchors = create_anchor_boxes(anchors)
+
+    def run(self, norm_boxes, labels, y_shape):
+        """
+        # Args
+            labels : list of integers
+            
+            y_shape : tuple
+                (grid_size, grid_size, nb_boxes, 4+1+nb_classes)
+            b_shape : tuple
+                (1, 1, 1, max_box_per_image, 4)
+        """
+        y = np.zeros(y_shape)
+        
+        # loop over objects in one image
+        for norm_box, label in zip(norm_boxes, labels):
+            best_anchor = self._find_anchor_idx(norm_box)
+
+            # assign ground truth x, y, w, h, confidence and class probs to y_batch
+            y += self._generate_y(best_anchor, label, norm_box, y_shape)
+        return y
+
+    def _find_anchor_idx(self, norm_box):
+        _, _, center_w, center_h = norm_box
+        shifted_box = np.array([0, 0, center_w, center_h])
+        return find_match_box(shifted_box, self.anchors)
+    
+    def _generate_y(self, best_anchor, obj_indx, box, y_shape):
+        y = np.zeros(y_shape)
+        grid_x, grid_y, _, _ = box.astype(int)
+        y[grid_y, grid_x, best_anchor, 0:4] = box
+        y[grid_y, grid_x, best_anchor, 4  ] = 1.
+        y[grid_y, grid_x, best_anchor, 5+obj_indx] = 1
+        return y
