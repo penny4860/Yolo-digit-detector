8180847c0cdca9960572df4f2aa3252b21409323
jeongjoonsup <penny4860@gmail.com>
2017-12-26 16:30:44 +0900

    test fail fixed

--- yolo/predict.py
+++ yolo/predict.py
@@ -1,7 +1,7 @@
 
 import json
 from yolo.utils.box import draw_boxes
-from yolo import YOLO
+from yolo.frontend import create_yolo
 import cv2
 
 def predict(image_path, weights_path, conf="config.json"):
@@ -9,14 +9,12 @@ def predict(image_path, weights_path, conf="config.json"):
     with open(conf) as config_buffer:
         config = json.loads(config_buffer.read())
 
-    # 1. Construct the model 
-    yolo = YOLO(config['model']['architecture'],
-                config['model']['labels'],
-                config['model']['input_size'],
-                config['model']['max_box_per_image'],
-                config['model']['anchors'])
-    # 2. Load the pretrained weights (if any) 
-    yolo.load_weights(weights_path)
+    yolo = create_yolo(config['model']['architecture'],
+                       config['model']['labels'],
+                       config['model']['input_size'],
+                       config['model']['max_box_per_image'],
+                       config['model']['anchors'],
+                       weights_path)
 
     ###############################
     #   Predict bounding boxes 
