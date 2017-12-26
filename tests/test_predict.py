c2e209e65cd7b777639498e3c6c07b0028450331
jeongjoonsup <penny4860@gmail.com>
2017-12-26 22:49:05 +0900

    test_predict modified

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,10 +1,11 @@
 
 
-from yolo.predict import predict
 import pytest
 import cv2
 import numpy as np
 import os
+from yolo.frontend import create_yolo
+from yolo.utils.box import BoundBox
 
 
 THIS_DIRECTORY = os.path.dirname(__file__)
@@ -14,45 +15,32 @@ SAMPLE_DIRECTORY = os.path.join(os.path.dirname(THIS_DIRECTORY),
 @pytest.fixture(scope='function')
 def setup_inputs(request):
     input_file = os.path.join(SAMPLE_DIRECTORY, "raccoon.jpg")
-    conf = os.path.join(SAMPLE_DIRECTORY, "config.json")
-    weights = os.path.join(SAMPLE_DIRECTORY, "mobilenet_raccoon.h5")
-    return input_file, conf, weights
+    image = cv2.imread(input_file)
+    return image
 
 
 @pytest.fixture(scope='function')
 def setup_outputs(request):
-    def _delete_file(fname):
-        if os.path.exists(fname):
-            os.remove(fname)
-    output_file = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected.jpg")
-    desired_file1 = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected_gt.jpg")
-    desired_file2 = os.path.join(SAMPLE_DIRECTORY, "raccoon_detected_gt2.jpg")
-    _delete_file(output_file)
-    def teardown():
-        _delete_file(output_file)
-    request.addfinalizer(teardown)
-    return output_file, desired_file1, desired_file2
-
+    desired_boxes = [BoundBox(0.50070397927, 0.585420268209, 0.680594700387, 0.758197716846, 0.576064, [ 0.57606441])]
+    return desired_boxes
 
 def test_predict(setup_inputs, setup_outputs):
-    def is_equal_image_file(output_file, desired_file):
-        output_img = cv2.imread(output_file)
-        desired_img = cv2.imread(desired_file)
-        return np.allclose(output_img, desired_img)
 
     # 1. Given 
-    input_file, conf, weights = setup_inputs
-    output_file, desired_file1, desired_file2 = setup_outputs
+    image = setup_inputs
+    desired_boxes = setup_outputs
     
-    # 2. When run main()
-    predict(input_file, weights, conf)
-
-    # 3. Should 
-    # Todo : Edit this !!!!!!!!!!!!!!!!!
-    # desired1 : saved in ubuntu 
-    # desired2 : saved in windows
-    assert(is_equal_image_file(output_file, desired_file1) == True or is_equal_image_file(output_file, desired_file2) == True)
+    # 2. When run
+    yolo = create_yolo("MobileNet",
+                       ["raccoon"],
+                       weights_path=os.path.join(SAMPLE_DIRECTORY, "mobilenet_raccoon.h5"))
+    boxes = yolo.predict(image)
     
+    # 3. should
+    for box, desired_box in zip(boxes, desired_boxes):    
+        variables = box.__dict__.keys()
+        for v in variables:
+            assert np.allclose(getattr(box, v), getattr(desired_box, v))
     
 if __name__ == '__main__':
     pytest.main([__file__, "-v", "-s"])
