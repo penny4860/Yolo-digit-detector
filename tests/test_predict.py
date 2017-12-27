40f66d6e43a3cb19f836d166fac0d8c9c501aae2
jeongjoonsup <penny4860@gmail.com>
2017-12-27 22:06:05 +0900

    test_predict modified

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -21,26 +21,25 @@ def setup_inputs(request):
 
 @pytest.fixture(scope='function')
 def setup_outputs(request):
-    desired_boxes = [BoundBox(0.50070397927, 0.585420268209, 0.680594700387, 0.758197716846, 0.576064, [ 0.57606441])]
-    return desired_boxes
+    desired_boxes = np.array([[104, 86, 546, 402]])
+    desired_probs = np.array([[ 0.57606441]])
+    return desired_boxes, desired_probs
 
 def test_predict(setup_inputs, setup_outputs):
 
     # 1. Given 
     image = setup_inputs
-    desired_boxes = setup_outputs
+    desired_boxes, desired_probs = setup_outputs
     
     # 2. When run
     yolo = create_yolo("MobileNet",
                        ["raccoon"],
                        weights_path=os.path.join(SAMPLE_DIRECTORY, "mobilenet_raccoon.h5"))
-    boxes = yolo.predict(image)
-    
+    boxes, probs = yolo.predict(image)
+
     # 3. should
-    for box, desired_box in zip(boxes, desired_boxes):    
-        variables = box.__dict__.keys()
-        for v in variables:
-            assert np.allclose(getattr(box, v), getattr(desired_box, v))
+    assert np.allclose(boxes, desired_boxes)
+    assert np.allclose(probs, desired_probs)
     
 if __name__ == '__main__':
     pytest.main([__file__, "-v", "-s"])
