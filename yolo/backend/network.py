0dca88bc606c0716c03ecc5a173ee0f842f20584
jeongjoonsup <penny4860@gmail.com>
2017-12-27 19:19:57 +0900

    feature module moved to utils pkg

--- yolo/backend/network.py
+++ yolo/backend/network.py
@@ -4,8 +4,7 @@ from keras.layers import Reshape, Conv2D, Input, Lambda
 import numpy as np
 import cv2
 
-# Todo : remove dependency 
-from yolo.backend.feature import create_feature_extractor
+from .utils.feature import create_feature_extractor
 
 
 class YoloNetwork(object):
