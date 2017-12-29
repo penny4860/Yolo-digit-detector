524cc1e98c72a03cde8bc5321886f0ed8d5ede12
jeongjoonsup <penny4860@gmail.com>
2017-12-29 19:23:53 +0900

    config format edited
    Conflicts:
    	config_raccoon.json
    	train_driver.py

--- train_driver.py
+++ train_driver.py
@@ -34,9 +34,26 @@ if __name__ == '__main__':
                        config['pretrained']['feature'])
     
     # 2. Load the pretrained weights (if any) 
-    # yolo.load_weights(config['pretrained']['full'])
+    yolo.load_weights(config['pretrained']['full'])
     
-    # 3. Parse the annotations 
+    # 3. warmup training
+    yolo.train(config['train']['train_image_folder'],
+               config['train']['train_annot_folder'],
+               config['train']['warmup']['nb_epoch'],
+               config['train']['saved_weights'],
+               config['train']['warmup']['batch_size'],
+               config["train"]["jitter"],
+               config['train']['learning_rate'], 
+               config['train']['train_times'],
+               config['train']['valid_times'],
+               config['train']['warmup']['nb_epoch'],
+               config['train']['valid_image_folder'],
+               config['train']['valid_annot_folder'])
+
+    # 4. Load the warmup trained weights
+    yolo.load_weights(config['train']['saved_weights'])
+    
+    # 5. actual training 
     yolo.train(config['train']['train_image_folder'],
                config['train']['train_annot_folder'],
                config['train']['nb_epoch'],
