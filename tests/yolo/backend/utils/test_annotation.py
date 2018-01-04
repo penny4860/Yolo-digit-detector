7a2639ef2fe8f0a250793cb287e9a47bbd0c03af
jeongjoonsup <penny4860@gmail.com>
2018-01-04 20:16:49 +0900

    test code applied

--- tests/yolo/backend/utils/test_annotation.py
+++ tests/yolo/backend/utils/test_annotation.py
@@ -6,12 +6,12 @@ import numpy as np
 from yolo.backend.utils.annotation import parse_annotation
 import yolo
 
-TEST_SAMPLE_DIR = os.path.join(yolo.PROJECT_ROOT, "tests", "dataset")
+TEST_SAMPLE_DIR = os.path.join(yolo.PROJECT_ROOT, "tests", "dataset", "svhn")
 
 @pytest.fixture(scope='function')
 def setup_inputs(request):
-    annotation_dir = os.path.join(TEST_SAMPLE_DIR, "raccoon_train_annotations//")
-    image_dir = os.path.join(TEST_SAMPLE_DIR, "raccoon_train_imgs//")
+    annotation_dir = os.path.join(TEST_SAMPLE_DIR, "anns//")
+    image_dir = os.path.join(TEST_SAMPLE_DIR, "imgs//")
     return annotation_dir, image_dir
 
 @pytest.fixture(scope='function')
@@ -30,7 +30,7 @@ def test_parse_annotation(setup_inputs, setup_expected_outputs):
     annotation_dir, image_dir = setup_inputs
     
     # When
-    annotations, seen_labels = parse_annotation(annotation_dir, image_dir)
+    annotations = parse_annotation(annotation_dir, image_dir)
     
     filenames, minmax_boxes, labels, seen_labels_ = setup_expected_outputs
     
