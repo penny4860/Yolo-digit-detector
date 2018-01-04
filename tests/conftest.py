5864d2a73f3400fc1486728bdf16a7be87521c94
jeongjoonsup <penny4860@gmail.com>
2018-01-04 19:30:40 +0900

    conftest.py added

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+
+import pytest
+from yolo.backend.utils.box import to_centroid
+
+# Todo : test_train duplicated code
+@pytest.fixture(scope='module')
+def setup_model_config(request):
+    model_config = {"architecture":         "MobileNet",
+                    "input_size":           288,
+                    "anchors":              [0.57273, 0.677385,
+                                             1.87446, 2.06253,
+                                             3.33843, 5.47434,
+                                             7.88282, 3.52778,
+                                             9.77052, 9.16828],
+                    "max_box_per_image":    10,        
+                    "labels":               ["1", "2", "3", "9"]}
+    return model_config
+@pytest.fixture(scope='module')
+def setup_centroid_true_boxes(request):
+    true_boxes = to_centroid(np.array([[246, 77, 327, 296],
+                                       [323, 81, 419, 300]]))
+    return true_boxes
--- tests/test_predict.py
+++ tests/test_predict.py
@@ -10,19 +10,6 @@ import yolo
 
 TEST_SAMPLE_DIR = os.path.join(yolo.PROJECT_ROOT, "tests", "dataset", "svhn")
 
-# Todo : test_train duplicated code
-@pytest.fixture(scope='function')
-def setup_model_config(request):
-    model_config = {"architecture":         "MobileNet",
-                    "input_size":           288,
-                    "anchors":              [0.57273, 0.677385,
-                                             1.87446, 2.06253,
-                                             3.33843, 5.47434,
-                                             7.88282, 3.52778,
-                                             9.77052, 9.16828],
-                    "max_box_per_image":    10,        
-                    "labels":               ["1", "2", "3", "9"]}
-    return model_config
 @pytest.fixture(scope='function')
 def setup_centroid_true_boxes(request):
     true_boxes = to_centroid(np.array([[246, 77, 327, 296],
