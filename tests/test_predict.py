b1489d73e33b510c81682f48b2049183fd3220db
jeongjoonsup <penny4860@gmail.com>
2018-01-04 19:23:28 +0900

    test_predict : changed to "svhn dataset"

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -5,17 +5,35 @@ import cv2
 import numpy as np
 import os
 from yolo.frontend import create_yolo
+from yolo.backend.utils.box import to_centroid, centroid_box_iou
 import yolo
 
 TEST_SAMPLE_DIR = os.path.join(yolo.PROJECT_ROOT, "tests", "dataset", "svhn")
 
+# Todo : test_train duplicated code
+@pytest.fixture(scope='function')
+def setup_model_config(request):
+    model_config = {"architecture":         "MobileNet",
+                    "input_size":           288,
+                    "anchors":              [0.57273, 0.677385,
+                                             1.87446, 2.06253,
+                                             3.33843, 5.47434,
+                                             7.88282, 3.52778,
+                                             9.77052, 9.16828],
+                    "max_box_per_image":    10,        
+                    "labels":               ["1", "2", "3", "9"]}
+    return model_config
+@pytest.fixture(scope='function')
+def setup_centroid_true_boxes(request):
+    true_boxes = to_centroid(np.array([[246, 77, 327, 296],
+                                       [323, 81, 419, 300]]))
+    return true_boxes
 
 @pytest.fixture(scope='function')
 def setup_inputs(request):
-    input_file = os.path.join(TEST_SAMPLE_DIR, "raccoon.jpg")
-    image = cv2.imread(input_file)
+    input_image_file = os.path.join(TEST_SAMPLE_DIR, "imgs", "1.png")
+    image = cv2.imread(input_image_file)
     return image
-
 
 @pytest.fixture(scope='function')
 def setup_outputs(request):
@@ -23,22 +41,36 @@ def setup_outputs(request):
     desired_probs = np.array([[ 0.57606441]])
     return desired_boxes, desired_probs
 
-def test_predict(setup_inputs, setup_outputs):
+def test_predict(setup_inputs, setup_outputs, setup_centroid_true_boxes, setup_model_config):
 
     # 1. Given 
     image = setup_inputs
+    model_config = setup_model_config
+
     desired_boxes, desired_probs = setup_outputs
     
     # 2. When run
-    yolo = create_yolo("MobileNet",
-                       ["raccoon"])
-    yolo.load_weights(os.path.join(TEST_SAMPLE_DIR, "mobilenet_raccoon.h5"))
+    # 1. Construct the model 
+    yolo = create_yolo(model_config['architecture'],
+                       model_config['labels'],
+                       model_config['input_size'],
+                       model_config['max_box_per_image'],
+                       model_config['anchors'])
+    
+    yolo.load_weights(os.path.join(TEST_SAMPLE_DIR, "mobile_288_weights.h5"))
     boxes, probs = yolo.predict(image)
 
-    # 3. should
-    assert np.allclose(boxes, desired_boxes)
-    assert np.allclose(probs, desired_probs)
-    
+    boxes = to_centroid(boxes)
+    true_boxes = setup_centroid_true_boxes
+
+    assert len(boxes) == 2
+    assert len(probs) == 2
+    assert np.allclose(np.argmax(probs, axis=1), [0, 3])
+    for box, true_box in zip(boxes, true_boxes):
+        iou = centroid_box_iou(box, true_box)
+        assert iou > 0.5
+
+
 if __name__ == '__main__':
     pytest.main([__file__, "-v", "-s"])
 
