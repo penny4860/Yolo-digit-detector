74fa13b0eb4985eebb67fee77e24a78b5f3fe1d2
jeongjoonsup <penny4860@gmail.com>
2018-01-05 21:54:14 +0900

    true tensor 변경 중

--- yolo/backend/loss.py
+++ yolo/backend/loss.py
@@ -51,8 +51,9 @@ class YoloLoss(object):
         def loss_func(y_true, y_pred):
             # 1. activate prediction & truth tensor
             # Todo : return tensor를 2개로 줄이자.
-            pred_tensor, true_box_xy, true_box_wh, true_box_conf, true_box_class = self._activator.run(y_true, y_pred)
+            pred_tensor, true_tensor = self._activator.run(y_true, y_pred)
             pred_box_xy, pred_box_wh, pred_box_conf, pred_box_class = pred_tensor[..., :2], pred_tensor[..., 2:4], pred_tensor[..., 4], pred_tensor[..., 5:]
+            true_box_xy, true_box_wh, true_box_conf, true_box_class = true_tensor[..., :2], true_tensor[..., 2:4], true_tensor[..., 4], true_tensor[..., 5]
             
             # 2. mask
             coord_mask = self._mask.create_coord_mask(y_true)
@@ -100,12 +101,18 @@ class _Activator(object):
     def run(self, y_true, y_pred):
         pred_box_xy, pred_box_wh, pred_box_conf, pred_box_class = self._activate_pred_tensor(y_pred)
         true_box_xy, true_box_wh, true_box_conf, true_box_class = self._activate_true_tensor(y_true, pred_box_xy, pred_box_wh)
+        
         # concatenate pred tensor
         pred_box_conf = tf.expand_dims(pred_box_conf, -1)
         pred_tensor = tf.concat([pred_box_xy, pred_box_wh, pred_box_conf, pred_box_class], axis=-1)
+        
+        # concatenate true tensor
+        true_box_conf = tf.expand_dims(true_box_conf, -1)
+        true_box_class = tf.expand_dims(true_box_class, -1)
+        true_tensor = tf.concat([true_box_xy, true_box_wh, true_box_conf, true_box_class], axis=-1)
 
         # Todo : many returns
-        return pred_tensor, true_box_xy, true_box_wh, true_box_conf, true_box_class
+        return pred_tensor, true_tensor
     
     def _activate_pred_tensor(self, y_pred):
         """
@@ -347,6 +354,11 @@ def test_activator(setup_y_true_tensor):
     activator = _Activator([0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828])
     pred_tensor, true_box_xy, true_box_wh, true_box_conf, true_box_class = activator.run(y_true, y_pred)
     print("================================================")
+    print(true_box_xy.shape, true_box_wh.shape, true_box_conf.shape, true_box_class.shape)
+    # (?, 13, 13, 5, 2) (?, 13, 13, 5, 2) (?, 13, 13, 5) (?, 13, 13, 5)
+    true_box_conf = tf.expand_dims(true_box_conf, -1)
+    true_box_class = tf.expand_dims(true_box_class, -1)
+    print(true_box_xy.shape, true_box_wh.shape, true_box_conf.shape, true_box_class.shape)
     print("================================================")
     
 
