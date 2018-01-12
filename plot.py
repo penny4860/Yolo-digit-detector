4ef0c40e29d6b788c118a5709c0e6784bd43ca23
jeongjoonsup <penny4860@gmail.com>
2018-01-12 18:24:36 +0900

    plot edited

--- plot.py
+++ plot.py
@@ -10,12 +10,12 @@ imgs = os.listdir(FOLDER)
 for i, fname in enumerate(imgs):
     path = os.path.join(FOLDER, fname)
     img = plt.imread(path)
-    plt.subplot(4, 5, i+1)
-    plt.title(fname)
+    plt.subplot(4, 6, i+1)
+#     plt.title(fname)
+    plt.axis('off')
     plt.imshow(img)
-plt.subplots_adjust(wspace=0.1, hspace=0.1)
+plt.subplots_adjust(left=0.01, right=0.99, wspace=0.05, hspace=0.05)
 plt.show()
-
 
 # IMG_FILE = os.path.join(FOLDER, "1.png")
 # 
